3c6e1ebf98412ad4c0dcaa3f6627edd97506ffde
edithntinyari <emothegi@gmail.com>
2020-06-15 10:49:05 +0300

    added quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DateCountPipe } from './date-count.pipe';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DateCountPipe
+    DateCountPipe,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,

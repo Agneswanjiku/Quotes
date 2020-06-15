afe21c9dac8b55906aae7b4bc340a1eb51d5c70a
edithntinyari <emothegi@gmail.com>
2020-06-15 14:56:03 +0300

    added date-counts pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule} from '@angular/forms';
+
 import { AppComponent } from './app.component';
-import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteComponent } from './quotes/quotes.component';
+import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
+import { UnderlineDirective } from './underline.directive';
+import { DateCountPipe, DateCountsPipe } from './date-counts.pipe';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { CountInputComponent } from './count-input/count-input.component';
-import { DateCountPipe } from './date-count.pipe';
-import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailComponent,
-    QuoteFormComponent,
-    CountInputComponent,
+    QuotesDetailComponent,
+    UnderlineDirective,
     DateCountPipe,
-    QuoteDetailsComponent
+    QuoteFormComponent,
+    DateCountsPipe
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ]
+    FormsModule,
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }

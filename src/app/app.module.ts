d3e9f819aa4d98a61d30e3b3e1a988cf6ca6c0a2
edithntinyari <emothegi@gmail.com>
2020-06-16 13:03:03 +0300

    added changes in files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { FormsModule} from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-import { DateCountPipe } from './date-counts.pipe';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { AppRoutingModule } from './app-routing.module';
-import { from } from 'rxjs';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    DateCountPipe,
+    QuoteDetailComponent,
     QuoteFormComponent,
-    QuoteDetailComponent
-
-
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
-
-
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
